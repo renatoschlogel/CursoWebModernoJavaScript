724e1e4c7d9c6e984cfaa4faab251d746e2d3e70
Renato Welinton Schlogel <rschlogel@hotmail.com>
2018-12-13 21:48:27 -0200

    Ajuste de diretorio

---
+++ funcoes/closure.js
@@ -0,0 +1,15 @@
+// Closure é o escopo de onde a funcao foi criada
+
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
