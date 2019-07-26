54cd7de441e4cf96ede41b016f1c27c738d79e8a
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-07-25 23:24:14 -0300

    Ajuste de exemplo de leitura de arquivos e diretorios

--- node/arquivoLeitura.js
+++ node/arquivoLeitura.js
@@ -10,6 +10,15 @@ console.log(conteudo);
 
 // assincrono
 fs.readFile(caminho, 'utf-8', (erro, dado) => {
-    const config = dado;
-    console.log('OOpaaa', config);
+    const config = JSON.parse(dado);
+    console.log('OOpaaa', config.db.host);
 })
+
+let config = require('./arquivo.json');
+
+console.log('OOpaaSimples assim:', config.db);
+
+fs.readdir(__dirname, (erro, arquivos)=>{
+    console.log('Conteundo Pasta');
+    console.log(arquivos);
+});
