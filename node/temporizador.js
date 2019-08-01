4acbfb55d5635a92f83d0aeb7a12cc905d462d20
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-01 00:33:12 -0300

    temporizador com regra por RecurrenceRule

--- node/temporizador.js
+++ node/temporizador.js
@@ -3,8 +3,16 @@ const tarefa1 = schedule.scheduleJob('*/1 * 0 * * 4', function(){
     console.log('Executando Tarefa 1!', new Date().getSeconds());
 });
 
-setTimeout(() => {
+setTimeout(function (){
     tarefa1.cancel();
-    console.log('Cancelando Tarefa 1');
-}, 150000);
+    console.log(' ** Cancelando Tarefa 1!');
+}, 5000);
 
+const regra = new schedule.RecurrenceRule();
+regra.dayOfWeek = [new schedule.Range(1.5)];
+regra.hour = 0;
+regra.second = 30;
+
+const tarefa2 = schedule.scheduleJob(regra, function () {
+    console.log('Executando Tarefa 2');
+});
