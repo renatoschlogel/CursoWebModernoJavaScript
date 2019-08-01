f33d63522c0b0819f86e5145cd068bc18090e43c
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-08-01 00:20:30 -0300

    Cancelando uma tarefa do scheduleJob

--- node/temporizador.js
+++ node/temporizador.js
@@ -2,3 +2,9 @@ const schedule = require('node-schedule');
 const tarefa1 = schedule.scheduleJob('*/1 * 0 * * 4', function(){
     console.log('Executando Tarefa 1!', new Date().getSeconds());
 });
+
+setTimeout(() => {
+    tarefa1.cancel();
+    console.log('Cancelando Tarefa 1');
+}, 150000);
+
